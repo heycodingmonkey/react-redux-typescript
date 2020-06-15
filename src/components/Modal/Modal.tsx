a0755727e694ddd79315997ee74c61a1284cebaa
milantexus <milan.perera@texussolutions.com>
2020-06-15 19:46:13 +0530

    adding form styles

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
+import React, { useState } from "react";
 //@material-ui/core icons
 import Clear from "@material-ui/icons/Clear";
+import DatePicker from "react-date-picker";
 // styles
 import "../../assets/components/modal.scss";
 
@@ -13,6 +12,12 @@ interface Props {
 
 // this will list down all the customers added
 export const Modal: React.FC<Props> = ({ handleModal }) => {
+  const [date, setDate] = useState<Date | Date[]>(new Date());
+
+  const setDateValue = (value: Date | Date[]) => {
+    setDate(value);
+  };
+
   return (
     <div className="modal-overlay">
       <div className="modal-wrapper">
@@ -23,6 +28,45 @@ export const Modal: React.FC<Props> = ({ handleModal }) => {
           <div className="icon-wrapper">
             <Clear onClick={handleModal} className="clear-icon" />
           </div>
+        </div>
+        <div className="modal-content">
+          <div className="content-left">
+            {" "}
+            <div className="field-wrapper">
+              <input
+                className="form-input"
+                type="text"
+                placeholder="First Name"
+              />
+            </div>
+            <div className="field-wrapper">
+              <input
+                className="form-input"
+                type="text"
+                placeholder="Second Name"
+              />
+            </div>
+            <div className="field-wrapper">
+              <input
+                className="form-input"
+                type="number"
+                placeholder="Mobile Number"
+              />
+            </div>
+          </div>
+          <div className="content-right">
+            <div className="field-wrapper">
+              <DatePicker
+                onChange={(e) => {
+                  setDateValue(e);
+                }}
+                value={date}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="modal-footer">
+          <button className="add-button">Add Customer</button>
         </div>
       </div>
     </div>
